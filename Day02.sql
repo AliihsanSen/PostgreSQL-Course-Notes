11221928618d4a8aba477311564377d79015d289
Alı Ihsan SEN <av.aliihsansen@gmail.com>
2022-09-04 00:01:35 +0300

    04.09.2022

--- Day02.sql
+++ Day02.sql
@@ -1,4 +1,5 @@
 --Personel isminde bir tablo oluşturalım
+
 create table personel(
 pers_id int,
 isim varchar(30),
@@ -7,30 +8,38 @@ maas int,
 sirket varchar(20),
 adres varchar(50)    
 );
+
 --Varolan personel tablosundan pers_id,sehir,adres fieldlarına sahip personel_adres adında yeni tablo olusturalım
+
 create table personel_adres
 as
 select pers_id, sehir, adres from personel;
 select * from personel
+
 -- DML --> Data Manupulation Lang.
 -- INSERT - UPDATE - DELETE
 --Tabloya veri ekleme, tablodan veri güncelleme ve silme işlemlerinde kullanılan komutlar
 --INSERT
+
 create table student
 (
 id varchar(4),
 st_name varchar(30),
 age int
 );
+
 INSERT into student VALUES ('1001','Ali Can',25);
 INSERT into student VALUES ('1002','Veli Can',35);
 INSERT into student VALUES ('1003','Ayse Can',45);
 INSERT into student VALUES ('1004','Derya Can',55);
+
 --Tabloya parçalı veri ekleme
+
 insert into student(st_name,age) values ('Murat Can',65);
 
 --DQL --> Data Query Lang.
 --SELECT
+
 select * from student;
 select st_name from student;
 
@@ -41,6 +50,7 @@ select * from student WHERE age>35;
 --Begin - Savepoint - rollback - commit
 --Transaction veritabanı sistemlerinde bir işlem başladığında başlar ve işlem bitince sona erer
 --Bu işlemler veri tabanı oluşturma, veri silme, veri güncelleme, veriyi geri getirme gibi işlemler olabilir
+
 CREATE TABLE ogrenciler2
 (
 id serial,
@@ -49,6 +59,7 @@ veli_isim VARCHAR(50),
 yazili_notu real       
 );
 Begin;
+
 insert into ogrenciler2 VALUES (default,'Ali Can','Hasan Can',75.5);
 insert into ogrenciler2 VALUES (default,'Canan Gül','Ayşe Şen',90.5);
 savepoint x;
@@ -109,11 +120,13 @@ Delete from ogrenciler
 
 select * from ogrenciler;
 TRUNCATE TABLE ogrenciler
+
 -- DDL - Data Definition Lang. 
 -- CREATE - ALTER - DROP
 -- ALTER TABLE --
 -- ALTER TABLE tabloda ADD, TYPE, SET, RENAME veya DROP COLUMNS işlemleri için kullanılır
 --Personel isminde bir tablo oluşturalım
+
 create table personel(
 pers_id int,
 isim varchar(30),
@@ -194,5 +207,6 @@ INSERT into adresler VALUES(NULL,'ağa Sok','30. cad','Antep');
 SELECT * FROM calisanlar, adresler WHERE calisanlar.id= adresler.adres_id
 
 Drop table calisanlar;
+
 --Parent tabloyu yani primary key olan tabloyu silmek istediğimizde tabloyu silmez
 --Önce child tabloyu silmemiz gerekir.
